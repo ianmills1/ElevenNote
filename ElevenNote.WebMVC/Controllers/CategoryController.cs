e7df1f0b5138c752eee238ae3e5b28150d8fd79b
Ian Mills <iandmills1@gmail.com>
2019-11-07 10:28:21 -0500

    beginning to add api

---
+++ ElevenNote.WebMVC/Controllers/CategoryController.cs
@@ -0,0 +1,113 @@
+﻿using ElevenNote.Models;
+using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNote.WebMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            var service = CreateCategoryService();
+            return View(service.GetCategories());
+        }
+
+        private CategoryService CreateCategoryService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new CategoryService(userId);
+            return service;
+        }
+
+        //GET
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CategoryCreate model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            var service = CreateCategoryService();
+
+            if (service.CreateCategory(model))
+            {
+                TempData["SaveResult"] = "Your category was created.";
+                return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError("", "Your category could not be created.");
+
+            return View(model);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var service = CreateCategoryService();
+            var detail = service.GetCategoryById(id);
+            var model =
+                new CategoryEdit
+                {
+                    CategoryId = detail.CategoryId,
+                    Name = detail.Name,
+                };
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, CategoryEdit model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            if (model.CategoryId != id)
+            {
+                ModelState.AddModelError("", "ID Mismatch");
+                return View(model);
+            }
+
+            var service = CreateCategoryService();
+
+            if (service.UpdateCategory(model))
+            {
+                TempData["SaveResult"] = "The category was updated.";
+                return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError("", "The category could not be updated.");
+            return View(model);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var svc = CreateCategoryService();
+            var model = svc.GetCategoryById(id);
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteCategory(int id)
+        {
+            var service = CreateCategoryService();
+
+            service.DeleteCategory(id);
+
+            TempData["SaveResult"] = "The category was deleted";
+
+            return RedirectToAction("Index");
+        }
+    }
+}
